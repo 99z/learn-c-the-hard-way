35fac4ef72b92c94e22b5cd7e2a6ea3066057612
niconan <nick.hax@gmail.com>
2016-02-04 19:40:14 -0500

    chapter 1 complete

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("This is me reviewing C.");
+  puts("A poet and I didn't even know it.");
+
+  return 0;
+}
